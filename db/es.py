a76d959e003aabf956f3eef54abdcf6dfe22fd3b
taoyahui <taoyahui>
2018-12-26 23:08:23 +0800

    modify

--- db/es.py
+++ db/es.py
@@ -26,11 +26,11 @@ class es_connector():
 
     # def conn(self):
 
-    def join_url(self, suffix):
+    def join_url(suffix):
         urls = [base_url, suffix]
         return '/'.join(urls)
 
-    def get(self, suffix):
+    def get(suffix):
         urls = [base_url, suffix]
         get_url = '/'.join(urls)
         res = requests.get(get_url, headers=header)
@@ -40,6 +40,16 @@ class es_connector():
     def post(self, suffix, data):
         get_url = self.join_url(suffix)
         results = requests.post(get_url, json.dumps(data), headers=header)
+        return results
+
+    def bulk(self, datas):
+        get_url = self.join_url('_bulk')
+        # print(get_url)
+        upload_data = ''
+        for data in datas:
+            upload_data += json.dumps(data) + '\n'
+        # print(upload_data)
+        results = requests.post(get_url, upload_data, headers=header)
         return results
 
 
--- scrapyspider/scrapyspider/spiders/cocoachina.py
+++ scrapyspider/scrapyspider/spiders/cocoachina.py
@@ -4,7 +4,8 @@ sys.path.append('..')
 import scrapy
 import json
 import common.utils
-
+import db.es
+import time
 
 class CocoachinaSpider(scrapy.Spider):
     name = 'cocoachina'
@@ -13,19 +14,30 @@ class CocoachinaSpider(scrapy.Spider):
 
     def parse(self, response):
         infos = []
+        es_conn = db.es.es_connector
         info_titles = response.xpath('//div[@class="yyinfoblock"]/a/div[@class="yyinfoblockinfo fr"]/h3/text()').extract()
         info_link = response.xpath('//div[@class="yyinfoblock"]/a/@href').extract()
+        cur_time = int(time.time())
         for index, title in enumerate(info_titles):
             infos.append({
-                'title':title,
-                'link': info_link[index]
+                "create": {
+                    "_index": "cocoachina",
+                    "_type": "_doc",
+                    "_id": cur_time + index
+                }
             })
+            infos.append({
+                'title': title,
+                'link': info_link[index],
+                'date': time.strftime('%Y-%m-%d')
+            })
+        es_conn.bulk(es_conn, infos)
         print()
         print()
-        common.utils.print_f(json.dumps(infos))
+        # common.utils.print_f(json.dumps(infos))
         print()
         print()
-        print(infos)
+        # print(infos)
         print()
         print()
-        print('djfkljsdklfjakldfjaklfjadslkfjalkfd')
+        # print('djfkljsdklfjakldfjaklfjadslkfjalkfd')
